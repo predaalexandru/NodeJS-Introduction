8eeb656122ced9f49ead99449f0b7eb6adc2a66e
Preda Alexandru <alexpreda95@yahoo.com>
2018-08-13 18:33:00 +0300

    Made a read file server

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,8 @@
 var http = require('http');
+var uc = require('upper-case');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Salutare, sunt Alex!');
+    res.write(uc('Salutare, sunt Alex!'));
     res.end();
 }).listen(8080);
